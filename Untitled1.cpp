2c40c81b263f736f2d75b0d4eb2af14a803dd8d3
Mohid23-tech <f2024266879@umt.edu.pk>
2026-01-16 16:06:04 +0500

    tasbeeh counter class
    yhn pa hum na phely head ko null ka equal rakha ha kyun ka starting point null ha
    phr hum na ek add zikar ka name sa function banaya ha ghn pa new node bn ka zikar add hota rhy gha
    hum na ek or function banaya ha jo zikr ki history bata ha
    or last ma ek destructor banya ha taka Program memory allocate to kar leta hai, lekin use free nahi karta.

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -10,3 +10,39 @@ struct ZikrNode {
     ZikrNode(string z, int c) : zikr(z), count(c), next(NULL) {}
 };
 
+// Linked List for Tasbeeh Counter
+class TasbeehCounter {
+private:
+    ZikrNode* head;
+public:
+    TasbeehCounter() : head(NULL) {}
+    
+    void addZikr(string zikr, int count) {
+        ZikrNode* newNode = new ZikrNode(zikr, count);
+        newNode->next = head;
+        head = newNode;
+        cout << "Added Zikr: " << zikr << " (Count: " << count << ")" << endl;
+    }
+    
+    void displayZikrHistory() {
+        if (!head) {
+            cout << "No Zikr history." << endl;
+            return;
+        }
+        cout << "\nZikr History:\n";
+        ZikrNode* temp = head;
+        while (temp) {
+            cout << temp->zikr << ": " << temp->count << " times" << endl;
+            temp = temp->next;
+        }
+    }
+    
+    ~TasbeehCounter() {
+        while (head) {
+            ZikrNode* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
