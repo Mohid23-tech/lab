00f6b806c36f5a0040ae3c7d33d5ae250d930879
Mohid23-tech <f2024266879@umt.edu.pk>
2026-01-16 15:55:44 +0500

    link list node
    tassbeeh counter ka liya node banya ha yhn pa or uska constructor call kia ha

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,3 +2,11 @@
 #include <string>
 using namespace std;
 
+// Node for Linked List (Tasbeeh Counter)
+struct ZikrNode {
+    string zikr;
+    int count;
+    ZikrNode* next;
+    ZikrNode(string z, int c) : zikr(z), count(c), next(NULL) {}
+};
+
